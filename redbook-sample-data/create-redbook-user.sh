ec89b32edaedc66ad6994fa1b9cc2889af195aa0
Neil Patterson <neil.patterson@nl.ibm.com>
2022-08-24 13:36:19 +0200

    updated sql for connection to database

--- redbook-sample-data/create-redbook-user.sh
+++ redbook-sample-data/create-redbook-user.sh
@@ -19,7 +19,7 @@ if [ $(id -u) -eq 0 ]; then
         source /home/db2inst1/.bashrc
 
         echo "Connecting to the bank database\n"
-        db2 connect to database bank as user db2inst1 password password
+        db2 connect to bank as user db2inst1 password password
 
 
         echo "granting the redbook user permissions\n"
