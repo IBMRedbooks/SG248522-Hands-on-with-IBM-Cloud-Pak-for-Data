7f39654bb32cc863e0063f7faf00c4a31f386108
Neil Patterson <neil.patterson@nl.ibm.com>
2022-08-25 10:19:11 +0200

    Added code to create a schema for demoing BI reporting  capability

---
+++ redbook-sample-data/create-reporting-mart-schema.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ $(id -u) -eq 0 ]; then
+        
+        source /home/db2inst1/.bashrc
+
+        echo "Connecting to the bank database\n"
+        db2 connect to bank user db2inst1 using password
+
+        echo "Executing the lineage settings script\n"
+        db2 CREATE SCHEMA REPORTINGMART
+        exit 0
+else
+        echo "Only root may add a user to the system."
+        exit 2
+fi
