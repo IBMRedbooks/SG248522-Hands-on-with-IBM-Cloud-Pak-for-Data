d595c47713e89f8d9ced44d47cfc83066e349043
Neil Patterson <neil.patterson@nl.ibm.com>
2022-08-25 10:01:37 +0200

    fixed ubg in create view script

--- redbook-sample-data/create-view.sh
+++ redbook-sample-data/create-view.sh
@@ -8,7 +8,7 @@ if [ $(id -u) -eq 0 ]; then
         db2 connect to bank user db2inst1 using password
 
         echo "Executing the lineage settings script\n"
-        db2 -tvmf lineage-settings.sql
+        db2 -tvmf create-view.sql
         exit 0
 else
         echo "Only root may add a user to the system."
